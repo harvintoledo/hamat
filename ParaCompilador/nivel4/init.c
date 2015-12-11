bf29b0763dd40e6e540aad1be2bdc49dc4690dfe
Harvin Toledo <harvintoledo@gmail.com>
2015-12-11 17:17:29 -0600

    Agregada funcion init.c que contiene tabla de constante y un arreglo a funciones con sus respectivos valores de punteros.

---
+++ ParaCompilador/nivel4/init.c
@@ -0,0 +1,49 @@
+#include "hoc.h"
+#include "y.tab.h"
+#include "math.h"
+
+extern double Log(), Log10(), Exp(), Sqrt(), integer();
+
+static struct {
+	char *name;
+	double cval;
+} consts[] = {
+	"PI", 3.1416,
+	"E", 2.7182,
+	"GAMMA", 0.577,
+	"DEG", 57.2957
+	"PHI", 1.61803,
+	0, 0
+};
+
+static struct {
+	char *name;
+	double (*func)();
+} builtins[] {
+	"sin", sin,
+	"cos", cos,
+	"atan", atan,
+	"log", Log,
+	"log10", Log10,
+	"exp", Exp,
+	"sqrt", Sqrt,
+	"int", integer,
+	"abs", fabs,
+	0, 0
+};
+
+init() {
+	int i;
+	Symbol *s;
+	
+	for(i = 0; consts[i].name; i++)
+		install(consts[i].name, VAR, consts[i].cval);
+	
+	for(i = 0; builtins[i].name; i++) {
+		s = install(builtins[i].name, BLTIN, 0.0);
+		s->u.ptr = builtins[i].func;
+	}
+}
+
+
+
