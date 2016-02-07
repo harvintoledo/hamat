340560edc550bda2228f439c378db00c0914a7c5
Jorge Potosme <fedoral10@gmail.com>
2016-02-07 16:45:36 -0600

    Agregado template java con maven

---
+++ ParaCompilador/OccamMavenTemplate/src/main/java/org/occammaventemplate/Memoria.java
@@ -0,0 +1,24 @@
+package org.occammaventemplate;
+
+public class Memoria {
+    private int indice;
+    private int contenido;
+
+    
+    public int getIndice() {
+        return indice;
+    }
+
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    public int getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(int contenido) {
+        this.contenido = contenido;
+    }
+    
+}
---
+++ ParaCompilador/OccamMavenTemplate/src/main/java/org/occammaventemplate/main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.occammaventemplate;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Potosme
+ */
+public class main {
+    public static void main(String argvs[])
+    {
+    Analizador a = new Analizador();
+    
+        try {
+            a.main(argvs);
+        } catch (IOException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
