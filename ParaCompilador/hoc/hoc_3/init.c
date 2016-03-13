e212f581832560e76e5c92696e01ec263c0e6290
Harvin Toledo <harvintoledo@gmail.com>
2016-03-13 12:14:30 -0600

    Agregado operador de igualdad a arreglo de funciones e identificadores builtins en init.c, agregado parámetro que faltaba a función install en init.c, agregado campo de puntero a funcion de arreglo de funciones builtins a estructura s de tipo Symbol.

--- ParaCompilador/hoc/hoc_3/init.c
+++ ParaCompilador/hoc/hoc_3/init.c
@@ -5,44 +5,45 @@
 extern double Log(), Log10(), Exp(), Sqrt(), integer();
 
 static struct {
-	char *name;
-	double cval;
+    char *name;
+    double cval;
 } consts [] = {
-	"PI",    3.14159265358979323846, /* Arquimide's constant */
-	"E",     2.71828182845904523536, /* Euler's number */
-	"GAMMA", 0.57721566490153286060, /* Euler's constant */
-	"DEG",  57.29577951308232087680, /* 1 radian by degress */
-	"PHI",   1.61803398874989484820, /* Golden ratio */
-	0, 0
+    "PI",    3.14159265358979323846, /* Arquimide's constant */
+    "E",     2.71828182845904523536, /* Euler's number */
+    "GAMMA", 0.57721566490153286060, /* Euler's constant */
+    "DEG",  57.29577951308232087680, /* 1 radian by degress */
+    "PHI",   1.61803398874989484820, /* Golden ratio */
+    0, 0
 };
 
 static struct {
-	char *name;
-	double (*func)();
-} builtins [] {
-	"sin", sin,
-	"cos", cos,
-	"atan", atan,
-	"log", Log,
-	"log10", Log10,
-	"exp", Exp,
-	"sqrt", Sqrt,
-	"int", integer,
-	"abs", fabs,
-	0, 0
+    char *name;
+    double (*func)();
+} builtins [] = {
+    "sin", sin,
+    "cos", cos,
+    "atan", atan,
+    "log", Log,
+    "log10", Log10,
+    "exp", Exp,
+    "sqrt", Sqrt,
+    "int", integer,
+    "abs", fabs,
+    0, 0
 };
 
 init () { /* Install constants and built ins in table */
-	int i;
-	Symbol *s;
-	
-	for( i = 0; consts[i].name, i++) {
-		install(consts[i].name, VAR, consts[i].cval);
-	}
-	
-	for( i = 0; builtins[i].name; i++) {
-		s = install(builtins[i].name, BLTIN)
-	}
+    int i;
+    Symbol *s;
+    
+    for( i = 0; consts[i].name; i++) {
+        install(consts[i].name, VAR, consts[i].cval);
+    }
+    
+    for( i = 0; builtins[i].name; i++) {
+        s = install(builtins[i].name, BLTIN, 0.0);
+		s->u.ptr = builtins[i].func;
+    }
 }
 
 
